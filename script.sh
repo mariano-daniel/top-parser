e388dd5297b533fdcfe8d1938bb25e644a85df26
Mariano Gedisman <60707523+mariano-daniel@users.noreply.github.com>
2022-04-19 00:32:37 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,11 +6,18 @@ string=$(ps aux -c | head -n 2 | awk '{print $2,$3,$11}') #I just found out this
 cpu_val=$(ps aux -c | awk 'NR==2 {print int($3)}') #Get the CPU with this string and save it to a var.
 
 
-#If loop to check if last 3 cpu readings is over 60%
+#If loop to check if last 3 cpu readings is over 50%
 
 counter=0
-while [ $cpu_val -le 60 ]
-        do
+while [[ $counter -eq 0 ]]
+do
+        if [[ $cpu_val -le 60 ]]
+        then
         cpu_val=$(ps aux -c | awk 'NR==2 {print int($3)}') #Get the CPU with this string and save it to a var.
         echo $cpu_val
-        done
+        
+        else
+        echo "conchas!"
+
+fi
+done
