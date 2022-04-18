7f0caca5d2166e937609e561a403e16ba349ac6d
Mariano Gedisman <60707523+mariano-daniel@users.noreply.github.com>
2022-04-19 00:11:55 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,16 @@
-string=$(ps aux -c | head -n 2 | awk '{print $2,$3,$11}')
+#! /bin/bash
+
+
+string=$(ps aux -c | head -n 2 | awk '{print $2,$3,$11}') #I just found out this can be fixed only with awk, To do: Get rid of head, awk can limit the lines with NR
+
+cpu_val=$(ps aux -c | awk 'NR==2 {print int($3)}') #Get the CPU with this string and save it to a var.
+
+
+#If loop to check if last 3 cpu readings is over 60%
+
+counter=0
+while [ $cpu_val -le 60 ]
+        do
+        cpu_val=$(ps aux -c | awk 'NR==2 {print int($3)}') #Get the CPU with this string and save it to a var.
+        echo $cpu_val
+        done
